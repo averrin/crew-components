d517e48ebce2d52c4fa6d271b027e5db9e5606d2
averrin <averrin@gmail.com>
2022-10-18 19:45:58 +0200

    update

--- components/IconButton.svelte
+++ components/IconButton.svelte
@@ -5,11 +5,13 @@
   export let type = "outline";
   export let cls;
   export let disabled = false;
+  export let style;
+  export let loading;
 
   import { createEventDispatcher } from "svelte";
   const dispatch = createEventDispatcher();
   function click(e) {
-    if (!disabled) {
+    if (!disabled && !loading) {
       dispatch("click", e);
     }
   }
@@ -19,7 +21,8 @@
   class="ui-btn ui-btn-square ui-btn-{size} ui-btn-{type} {cls}"
   class:ui-btn-disabled={disabled}
   {title}
+  {style}
   on:click={(e) => click(e)}
 >
-  <iconify-icon {icon} />
+  <iconify-icon icon={loading ? "line-md:loading-twotone-loop" : icon} />
 </button>
