2a3ec4553eef131fc3e78ce9b19e16d98bbf89fa
averrin <averrin@gmail.com>
2022-11-13 16:35:43 +0100

    update

--- helpers.js
+++ helpers.js
@@ -148,11 +148,11 @@ export default function initHelpers(mid, color, settings) {
   console.log("Crew components inited", mid)
 }
 
-export let setting = (key, val) => {
+export let setting = async (key, val) => {
   if (val === undefined) return game.settings.get(moduleId, key);
   else {
     // logger.info(`Writing ${moduleId}.${key} = ${JSON.stringify(val)}`);
-    game.settings.set(moduleId, key, val);
+    await game.settings.set(moduleId, key, val);
   }
 };
 
